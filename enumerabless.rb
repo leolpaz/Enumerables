1e22b65d37fc146f3c3f27d3e7b51573b0f53183
leolpaz <leonardolpaz95@gmail.com>
2021-03-08 16:17:46 -0300

    Finish my_count

--- enumerabless.rb
+++ enumerabless.rb
@@ -23,19 +23,21 @@ module Enumerable
   end
   
   def my_select
-  i = 0
-  arr = to_a
-  temp_arr = []
-  while i < arr.length
-    temp_arr.push(arr[i]) if yield arr[i]
-    i += 1
-  end
-  temp_arr
+    i = 0
+    arr = to_a
+    temp_arr = []
+    return false unless block_given?    
+    while i < arr.length
+      temp_arr.push(arr[i]) if yield arr[i]
+      i += 1
+    end
+    temp_arr
   end 
 
   def my_all?
     i = 0
     arr = to_a
+    return false unless block_given?  
     while i < arr.length
       return false unless yield arr[i]
       i += 1
@@ -46,6 +48,7 @@ module Enumerable
   def my_any?
     i = 0
     arr = to_a
+    return false unless block_given?
     while i < arr.length
       return true if yield arr[i]
       i += 1
@@ -56,11 +59,27 @@ module Enumerable
   def my_none?
     i = 0
     arr = to_a
+    return false unless block_given?
     while i < arr.length
       return false if yield arr[i]
       i += 1
     end
     return true
+  end
+
+  def my_count(param = nil)
+    i = 0
+    arr = to_a
+    counter = 0
+    my_each{|number| counter += 1 if number == param} unless param.nil?
+    return counter unless param.nil?
+    return arr.length unless block_given?
+    counter = 0
+    while i < arr.length
+      counter += 1 if yield arr[i]
+      i += 1
+    end
+    counter
   end
 end
 
@@ -75,5 +94,11 @@ a.my_all?{|number|}
 a.my_any?{|number|}
 
 a.my_none?{|number|}
+
+p a.my_count{|number|}
+
+
+
+
 
 
