51e2346f9cd6c04af6892a5c8116995d6b744524
leolpaz <leonardolpaz95@gmail.com>
2021-03-09 14:48:03 -0300

    Fix my_each not returning anything

--- lib/enumerables.rb
+++ lib/enumerables.rb
@@ -3,18 +3,17 @@
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
-
     i = 0
     arr = to_a
     while i < arr.length
       yield arr[i]
       i += 1
     end
+    self
   end
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
-
     i = 0
     arr = to_a
     while i < arr.length
@@ -69,7 +68,6 @@ module Enumerable
 
     while i < arr.length
       return false if yield arr[i]
-
       i += 1
     end
     true
